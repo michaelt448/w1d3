ff9980ff3613ee07bf6a84ecd1096c7b9a80ecc4
Michael Tilman <mikegithub77@gmail.com>
2019-05-01 18:45:51 +0000

    countLetters.js was returned to have functionality of counting the letters while countLetterPositions.js gives the letter positions

--- countLetterPositions.js
+++ countLetterPositions.js
@@ -3,10 +3,10 @@ function countLetters(word) {
   var noSpacesWord = word.split(" ").join("");
   for(let i = 0; i< noSpacesWord.length ; i++) {
     if(Object.keys(count).lastIndexOf(noSpacesWord[i]) === -1) {
-      count[noSpacesWord[i]] = 1;
+      count[noSpacesWord[i]] = [i];
     }
     else {
-      count[noSpacesWord[i]] ++;
+      count[noSpacesWord[i]].push(i);
     }
   }
   return count;
